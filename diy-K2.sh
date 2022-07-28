758f205cbe8ef16fdfe2a40ae5dcbe37e9372f1b
Hongjie <37176899+Carever@users.noreply.github.com>
2022-07-28 21:24:41 +0800

    Update diy-K2.sh

--- diy-K2.sh
+++ diy-K2.sh
@@ -5,7 +5,7 @@ sed -i 's/OpenWrt/K2/g' package/base-files/files/bin/config_generate
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 # 更改主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
