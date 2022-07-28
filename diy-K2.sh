6ea511cf664dc1bd94a63635aa4aece877a73dda
Hongjie <37176899+Carever@users.noreply.github.com>
2022-07-28 11:49:54 +0800

    Update diy-K2.sh

--- diy-K2.sh
+++ diy-K2.sh
@@ -20,4 +20,11 @@ sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai
 
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+# 修改ua2f的内核
+target=$(grep "^CONFIG_TARGET" .config --max-count=1 | awk -F "=" '{print $1}' | awk -F "_" '{print $3}')
+for configFile in $(ls target/linux/$target/config*)
+do
+    echo -e "\nCONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> $configFile
+done
