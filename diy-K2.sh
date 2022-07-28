d3969c0b14ea39a9c7259074448831a118a35bf3
Hongjie <37176899+Carever@users.noreply.github.com>
2022-07-29 00:30:30 +0800

    Update diy-K2.sh

--- diy-K2.sh
+++ diy-K2.sh
@@ -1,6 +1,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
+# Modify default THEME
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+
 # 修改ua2f的内核
 target=$(grep "^CONFIG_TARGET" .config --max-count=1 | awk -F "=" '{print $1}' | awk -F "_" '{print $3}')
 for configFile in $(ls target/linux/$target/config*)
