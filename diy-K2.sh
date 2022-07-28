9c389e3f206d2b96d314feb5f7942d74b421ccaa
Hongjie <37176899+Carever@users.noreply.github.com>
2022-07-28 20:32:56 +0800

    Update diy-K2.sh

--- diy-K2.sh
+++ diy-K2.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-#
-
-
 # 更改主机名
 sed -i 's/OpenWrt/K2/g' package/base-files/files/bin/config_generate
 
@@ -17,7 +6,6 @@ sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai
 
 # 更改主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
